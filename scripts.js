af88c3790068b53d84b6a1672c677fb312ee3c25
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-29 22:49:30 -0500

    add prime function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// FILL IN THE FUNCTIONS BELOW
-
 var sprintFunctions = {
   largestEl: function(arr){
     arr.sort(compare);
@@ -11,28 +9,64 @@ var sprintFunctions = {
     }
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function(str){  
+    strArr = str.split("")
+    revArr = [];
+
+    for(var i = strArr.length; i >= 0; i--){
+     revArr.push(strArr[i]);  
+    }
+   return revArr.join("");
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(str){  
+    str = str.split(" ")
+    for(var i = 0; i < str.length; i++){
+      str[i][0].toUpperCase();
+    }
   },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
+  compareArrays: function(arr1, arr2){ 
+    arr1 = arr1.sort();
+    arr2 = arr2.sort();
+
+    
   },
 
-  fizzBuzz: function(){  
-    // your code here
+  fizzBuzz: function(number){  
+    var arr = [];
+
+    for (i = 1; i <= number; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        arr.push("FIZZBUZZ");
+      }
+      else if (i % 3 === 0) {
+        arr.push("FIZZ");
+      }
+      else if (i % 5 === 0) {
+        arr.push("BUZZ");
+      }
+      else {
+        arr.push(i);
+      };
+    };
+    return arr;
   },
 
   myMap: function(){  
     // your code here
   },
 
-  primes: function(){  
-    // your code here
+  primes: function(n){  
+    primeArr = []
+      for(var i = 2; i < n; i++){
+        for(var j = 2; j < i; j++){
+          if(i % j == 0)
+            primeArr.push(i)
+        }
+      }
+  
+    
   },
 }
+
